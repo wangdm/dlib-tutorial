4f78a2de7ff09bd86a292902ef60f69877cb2825
wangdm <Wang411151499>
2020-07-28 20:25:39 +0800

    add opencv library

---
+++ source/video_capture.cpp
@@ -0,0 +1,27 @@
+//
+// Created by wangdm on 2020/7/25.
+//
+
+#include <unistd.h>
+
+#include "opencv2/videoio.hpp"
+#include "opencv2/highgui.hpp"
+
+using namespace cv;
+
+int main(int argc, char *argv[]) {
+//    VideoCapture cap(0);
+//    if (cap.isOpened()) {
+//        while (1) {
+//            Mat frame;
+//            cap >> frame;
+//            imshow("video", frame);
+//            usleep(30 * 1000);
+//            printf("get video\n");
+//        }
+//        cap.release();
+//    } else {
+//        printf("Camera is opened\n");
+//    }
+    im
+}
---
+++ source/window.cpp
@@ -0,0 +1,11 @@
+//
+// Created by wangdm on 2020/7/26.
+//
+
+#include <stdio.h>
+
+using namespace cv;
+
+int main(int argc, char *argv[]) {
+
+}
